5fafb394d190519a8777b6b974c94571fb4c7883
dianyuan <dianyuan0@gmail.com>
2017-12-15 13:40:55 -0500

    commit 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import allReducers from './Dev/Reducers/index'
+import App from './Dev/Components/App'
 
-const styles = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center',
-};
+const store = createStore(allReducers);
 
-const App = () => (
-  <div style={styles}>
-    <Hello name="CodeSandbox" />
-    <h2>Start editing to see some magic happen {'\u2728'}</h2>
-  </div>
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root')
 );
-
-render(<App />, document.getElementById('root'));
